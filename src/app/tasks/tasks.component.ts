eeb4e2579ee47a784c7cc9160045d15a6e3e888c
Seyedreza Safavi <rezasfv36@gmail.com>
2024-08-25 20:29:09 +0200

    add dynamic tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,39 @@
 import { Component,Input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
+
+
+
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input({required:true}) selectedName!:string;
+  @Input({required:true}) name!:string;
+  @Input({required:true}) userId!:string;
+
+  tasks=[{
+    id: 't1',
+    userId: 'u1',
+    title: 'Master Angular u1',
+    summary:
+      'Learn all the basic and advanced features of Angular & how to apply them.',
+    dueDate: '2025-12-31',
+  },
+  {
+    id: 't1',
+    userId: 'u3',
+    title: 'Master Angular u3',
+    summary:
+      'Learn all the basic and advanced features of Angular & how to apply them.',
+    dueDate: '2025-12-31',
+  }
+]
+
+get selectedUserTasks(){
+  return this.tasks.filter((task)=>task.userId === this.userId)
+}
 }
